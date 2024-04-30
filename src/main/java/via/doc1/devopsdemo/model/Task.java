81133f05f6900f042e9bdca078f5402d7089647b
KaBence <kabaly.bence@gmail.com>
2024-04-30 16:31:28 +0200

    backend and database is working

---
+++ src/main/java/via/doc1/devopsdemo/model/Task.java
@@ -0,0 +1,86 @@
+package via.doc1.devopsdemo.model;
+
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity(name="Task")
+@Table(name="task")
+
+public class Task {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "team_member_id", insertable = false,updatable = false)
+    @JsonIgnore
+    private TeamMember teamMember;
+
+    public Task() {
+    }
+
+    public Task(TeamMember teamMember){
+        this.teamMember=teamMember;
+    }
+
+    public Task(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id.equals(task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
